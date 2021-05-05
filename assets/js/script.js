b577fe0dc517420d6060030bc3c487e8be5fa8b4
gabrisca <gabrielescarparo1985@gmail.com>
2021-05-05 15:48:02 +0200

    goBack e goNext

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 const app = new Vue({
   el: "#app",
   data:{
-    
+    counter: 0,
+    img: [
+      "assets/img/episodio I.png",
+      "assets/img/episodio II.jpg",
+      "assets/img/episodio III.jpg",
+      "assets/img/episodio IV.jpg",
+      "assets/img/episodio V.jpg",
+      "assets/img/episodio VI.jpg",
+      "assets/img/episodio VII.jpg",
+      "assets/img/episodio VIII.jpeg",
+      "assets/img/episodio IX.jpg",
+    ],
+  },
+  methods: {
+    goNext(){
+      this.counter++
+    },
+    goBack(){
+      this.counter--
+    },
   },
 })
